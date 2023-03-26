8628734ea88edd30a6d62ae7b2c1b4c070f8d389
gogi999 <g.petrovic9@gmail.com>
2023-03-26 15:51:19 +0200

    Added basic setup and created Navbar, Hero and Memory components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  Footer,
+  Hero,
+  Memory,
+  Navbar,
+} from './components';
+import {
+  hero,
+  memory,
+  navlinks,
+} from './data/travigodata';
+
+const App = () => {
+  return (
+    <>
+      <Navbar navlinks={navlinks} />
+      <Hero hero={hero} />
+      <Memory memory={memory} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
